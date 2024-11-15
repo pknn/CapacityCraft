19641cd5ebf5254d72b1389fe77fcaa8bfb77223
Pakanon Pantisawat <pakanon.pantisawat@agoda.com>
2024-11-15 22:35:18 +0700

    feat(Header): add click-to-copy functionality for room link in header to enhance user experience style(Header): improve header styling and hover effects for better visual feedback

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,29 @@
 import useRoomState from '../state/useRoomState';
 import useUserState from '../state/useUserState';
+import pushToClipboard from '../util/pushToClipboard';
 import Logo from './Logo';
 
 const Header = () => {
   const displayName = useUserState((state) => state.displayName);
   const roomId = useRoomState((state) => state.roomId);
 
+  const handleRoomClick = () => {
+    pushToClipboard(location.href);
+  };
+
   return (
-    <header className="container mx-auto flex max-w-screen-lg items-center justify-between py-4">
+    <header
+      onClick={handleRoomClick}
+      className="group container mx-auto flex max-w-screen-lg cursor-pointer items-center justify-between py-4"
+    >
       <Logo />
       {displayName && roomId ? (
         <span className="font-medium">
           {displayName}{' '}
-          <span className="group relative cursor-pointer text-stone-400 hover:text-stone-500 group-hover:*:block">
+          <span className="relative text-stone-400 group-hover:text-stone-500 group-hover:*:block">
             #{roomId}
-            <div className="absolute left-0 top-full mt-1 cursor-pointer opacity-0 transition-all duration-300 group-hover:opacity-100">
-              Copy link to clipboard
+            <div className="absolute left-0 top-full text-xs opacity-0 transition-all duration-300 group-hover:opacity-100">
+              Copy link to clipboard!
             </div>
           </span>
         </span>
