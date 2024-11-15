08da1cfc7790c250f2b9de0f65bfa17325032ca7
Pakanon Pantisawat <pakanon.pantisawat@agoda.com>
2024-11-15 22:02:41 +0700

    feat(Logo): make logo clickable to navigate to the home page for improved user experience

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,8 +1,17 @@
-const Logo = () => (
-  <div className="pl-0 p-4 text-2xl font-bold text-stone-800">
-    <div className="-mb-2">Capacity</div>
-    <div>Craft</div>
-  </div>
-);
+import { useNavigate } from 'react-router-dom';
+
+const Logo = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div
+      className="pl-0 p-4 text-2xl font-bold text-stone-800 cursor-pointer"
+      onClick={() => navigate('/')}
+    >
+      <div className="-mb-2">Capacity</div>
+      <div>Craft</div>
+    </div>
+  );
+};
 
 export default Logo;
