e070aa7729fbe4f577dac38966e4d2562d8abcf6
Pakanon Pantisawat <pakanon.pantisawat@agoda.com>
2024-11-20 17:01:06 +0700

    feat(Header.tsx): add clipboard functionality to copy room link with feedback message for improved user experience

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import pushToClipboard from '../util/pushToClipboard';
 import { AppState } from '../store';
 import Logo from './Logo';
 import { roomSelector } from '../store/roomSlice';
+import { useEffect, useState } from 'react';
 
 type Props = {
   roomId: string | undefined;
@@ -10,23 +11,39 @@ type Props = {
 };
 
 const Header = ({ roomId, displayName }: Props) => {
-  const handleRoomClick = () => {
+  const [copyContentMessage, setCopyContentMessage] = useState(
+    'Copy link to clipboard'
+  );
+  const [copied, setCopied] = useState(false);
+
+  useEffect(() => {
+    if (copied) {
+      setCopyContentMessage('Room Link Copied!');
+      setTimeout(() => {
+        setCopyContentMessage('Copy link to clipboard');
+        setCopied(false);
+      }, 3000);
+    }
+  }, [copied]);
+
+  const handleRoomIdClick = () => {
     pushToClipboard(location.href);
+    setCopied(true);
   };
 
   return (
-    <header className="container mx-auto flex max-w-screen-lg cursor-pointer items-center justify-between py-4">
+    <header className="container mx-auto flex max-w-screen-lg items-center justify-between py-4">
       <Logo />
       {displayName && roomId ? (
         <span className="group font-medium">
           {displayName}{' '}
           <span
-            className="relative text-stone-400 group-hover:text-stone-500 group-hover:*:block"
-            onClick={handleRoomClick}
+            className="relative cursor-pointer text-stone-400 group-hover:text-stone-500 group-hover:*:block"
+            onClick={handleRoomIdClick}
           >
             #{roomId}
             <div className="absolute left-0 top-full text-xs opacity-0 transition-all duration-300 group-hover:opacity-100">
-              Copy link to clipboard!
+              {copyContentMessage}
             </div>
           </span>
         </span>
