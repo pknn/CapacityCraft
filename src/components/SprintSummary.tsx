018895d65ddda117e55f292ccbe2913d362b1bd4
Pakanon Pantisawat <pakanon.pantisawat@agoda.com>
2024-11-20 20:26:20 +0700

    feat(SprintSummary): connect component to Redux store and display holidays and total working days feat(selectSprintSummary): add selectors to calculate total working days and holidays from state

--- src/components/SprintSummary.tsx
+++ src/components/SprintSummary.tsx
@@ -1,16 +1,35 @@
-const SprintSummary = () => (
+import { connect } from 'react-redux';
+import { AppState } from '../store';
+import {
+  selectHolidays,
+  selectTotalWorkingDays,
+} from '../store/selectors/selectSprintSummary';
+
+type StateProps = {
+  totalWorkingDays: number;
+  holidays: number;
+};
+
+type Props = StateProps;
+
+const SprintSummary = ({ totalWorkingDays, holidays }: Props) => (
   <div className="my-8">
     <h3 className="text-lg font-bold">Sprint Summary</h3>
     <table>
       <tbody>
         <tr>
+          <th className="pr-4 text-left font-medium">Public Holidays</th>
+          <td className="pr-4 text-right">{holidays}</td>
+          <td>Days</td>
+        </tr>
+        <tr>
           <th className="pr-4 text-left font-medium">Total Working Days</th>
-          <td className="pr-4 text-left">22</td>
+          <td className="pr-4 text-right">{totalWorkingDays}</td>
           <td>Days</td>
         </tr>
         <tr>
           <th className="text-left font-medium">Capacity</th>
-          <td className="pr-4 text-left">22</td>
+          <td className="pr-4 text-right">22</td>
           <td>SPs</td>
         </tr>
       </tbody>
@@ -18,4 +37,9 @@ const SprintSummary = () => (
   </div>
 );
 
-export default SprintSummary;
+const mapStateToProps = (state: AppState): StateProps => ({
+  totalWorkingDays: selectTotalWorkingDays(state),
+  holidays: selectHolidays(state),
+});
+
+export default connect(mapStateToProps)(SprintSummary);
